6ecac48f1655b0d7f58d22e552181d067ff40c77
Zeynep Palabıyık <125740535+zeynepplbyk@users.noreply.github.com>
2025-08-22 11:36:00 +0300

    Delete proje_6 directory

--- proje_6/dosya_yukle_form.php
+++
@@ -1,6 +0,0 @@
-<form action="dosya_yukle.php" method="post" enctype="multipart/form-data">
-    <label for="dosya">Dosya Seçin:</label>
-    <input type="file" id="dosya" name="dosya"><br><br>
-
-    <button type="submit">Dosya Yükle</button>
-</form>
